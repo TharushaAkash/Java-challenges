808ce93bbcc22b9314f835ea6eb4d14bb894ead2
Tharusha Akash <tharushaakasha22@gmail.com>
2025-03-20 17:55:58 +0530

    Find_Prime_Number

---
+++ FindPrimeNum.java
@@ -0,0 +1,52 @@
+package part1;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class FindPrimeNum {
+    private int number;
+    private int factorCount;
+    private int[] prime;
+
+
+    public FindPrimeNum(int number) {
+        this.number = number;
+        factorCount = 0;
+        prime = new int[number];
+    }
+
+    //Find prime number method
+    public void primeNum(){
+        for(int i = 1; i <= number; i++){
+            if(number % i == 0){
+                factorCount++;
+            }
+        }
+        if(factorCount == 2){
+            System.out.println("Factor Count:" + factorCount);
+            System.out.print("status: " + number + " is a prime number.");
+        }
+        else{
+            System.out.println("Factor Count:" + factorCount);
+            System.out.print("Status: " + number + " is not a prime number.");
+        }
+    }
+
+
+    //Main method
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Get user input
+        System.out.print("Enter the number : ");
+        int number = sc.nextInt();
+
+        System.out.print("\nNumber: " + number + "\n");
+
+        FindPrimeNum Num = new FindPrimeNum(number);
+
+        Num.primeNum();
+    }
+
+
+}
